c6ef9238da5e48982060e30e9f974869fcbd79cb
elitecodegroovy <liujigang@tisson.cn>
2021-02-26 09:41:30 +0800

    TIME

--- crates/core/thinking.rs
+++ crates/core/thinking.rs
@@ -3,11 +3,13 @@ use std::sync::mpsc;
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread;
-use std::time::Duration;
 use regex::internal::Input;
 use indicatif::{ProgressBar, ProgressStyle};
 use std::cmp::min;
-
+use std::time::{Duration, Instant};
+use chrono::{DateTime, Duration as Duration2, Utc, FixedOffset, Local, Timelike, Datelike,
+             NaiveDate, NaiveDateTime, NaiveTime};
+use chrono::format::ParseError;
 //pub fn do_rc() {
 //    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
 //    println!("rc a = {}", Rc::strong_count(&a));
@@ -1490,7 +1492,8 @@ fn download_pb() {
 
     let pb = ProgressBar::new(total_size);
     pb.set_style(ProgressStyle::default_bar()
-        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {bytes}/{total_bytes} ({eta})")
+        .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] \
+                      {bytes}/{total_bytes} ({eta})")
         .progress_chars("#>-"));
 
     while downloaded < total_size {
@@ -1502,6 +1505,112 @@ fn download_pb() {
 
     pb.finish_with_message("下载完成！");
     log::info!("下载完成");
+}
+fn day_earlier(date_time: DateTime<Utc>) -> Option<DateTime<Utc>> {
+    date_time.checked_sub_signed(Duration2::days(1))
+}
+
+fn convert_local_time_to_timezone() {
+    let local_time = Local::now();
+    let utc_time = DateTime::<Utc>::from_utc(local_time.naive_utc(), Utc);
+    let china_timezone = FixedOffset::east(8 * 3600);
+    let rio_timezone = FixedOffset::west(2 * 3600);
+    println!("Local time now is {}", local_time);
+    println!("UTC time now is {}", utc_time);
+    println!(
+        "Time in Hong Kong now is {}",
+        utc_time.with_timezone(&china_timezone)
+    );
+    println!("Time in Rio de Janeiro now is {}", utc_time.with_timezone(&rio_timezone));
+}
+
+fn showTime() -> Result<(), ParseError>{
+    let now = Utc::now();
+
+    let (is_pm, hour) = now.hour12();
+    println!(
+        "The current UTC time is {:02}:{:02}:{:02} {}",
+        hour,
+        now.minute(),
+        now.second(),
+        if is_pm { "PM" } else { "AM" }
+    );
+    println!(
+        "And there have been {} seconds since midnight",
+        now.num_seconds_from_midnight()
+    );
+
+    let (is_common_era, year) = now.year_ce();
+    println!(
+        "The current UTC date is {}-{:02}-{:02} {:?} ({})",
+        year,
+        now.month(),
+        now.day(),
+        now.weekday(),
+        if is_common_era { "CE" } else { "BCE" }
+    );
+    println!(
+        "And the Common Era began {} days ago",
+        now.num_days_from_ce()
+    );
+
+    let date_time: NaiveDateTime = NaiveDate::from_ymd(2017, 11, 12).and_hms(17, 33, 44);
+    println!(
+        "Number of seconds between 1970-01-01 00:00:00 and {} is {}.",
+        date_time, date_time.timestamp());
+
+    let date_time_after_a_billion_seconds = NaiveDateTime::from_timestamp(1_000_000_000, 0);
+    println!(
+        "Date after a billion seconds since 1970-01-01 00:00:00 was {}.",
+        date_time_after_a_billion_seconds);
+
+    let rfc2822 = DateTime::parse_from_rfc2822("Tue, 1 Jul 2003 10:52:37 +0200")?;
+    println!("{}", rfc2822);
+
+    let rfc3339 = DateTime::parse_from_rfc3339("1996-12-19T16:39:57-08:00")?;
+    println!("{}", rfc3339);
+
+    let custom = DateTime::parse_from_str("5.8.1994 8:00 am +0000", "%d.%m.%Y %H:%M %P %z")?;
+    println!("{}", custom);
+
+    let time_only = NaiveTime::parse_from_str("23:56:04", "%H:%M:%S")?;
+    println!("{}", time_only);
+
+    let date_only = NaiveDate::parse_from_str("2015-09-05", "%Y-%m-%d")?;
+    println!("{}", date_only);
+
+    let no_timezone = NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S")?;
+    println!("{}", no_timezone);
+
+    Ok(())
+}
+
+fn do_time() {
+    let start = Instant::now();
+    download_pb();
+    let duration = start.elapsed();
+
+    println!("Time elapsed in expensive_function() is: {:?}", duration.as_millis());
+
+    let now = Utc::now();
+    println!("{}", now);
+
+    let almost_three_weeks_from_now = now.checked_add_signed(Duration2::weeks(2))
+        .and_then(|in_2weeks| in_2weeks.checked_add_signed(Duration2::weeks(1)))
+        .and_then(day_earlier);
+    match almost_three_weeks_from_now {
+        Some(x) => println!("{}", x),
+        None => eprintln!("Almost three weeks from now overflows!"),
+    }
+    match now.checked_add_signed(Duration2::max_value()) {
+        Some(x) => println!("{}", x),
+        None => eprintln!("We can't use chrono to tell the time for the Solar System to complete more than one full orbit around the galactic center."),
+    }
+
+    // 切换时区
+    convert_local_time_to_timezone();
+    // 显示时间
+    showTime();
 }
 
 pub fn main_thinking() {
@@ -1541,5 +1650,5 @@ pub fn main_thinking() {
     do_fn_pointer();
 //    do_simple_web_listener();
     do_enum();
-    download_pb();
+    do_time();
 }
