b0d12d92c52ebad2948178ca791447b2993e1952
elitecodegroovy <elite_jigang@163.com>
2023-04-22 17:37:33 +0800

    init app project v3

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world!");
+    println!("cargo run: build and run");
+    println!("cargo check, c:    Analyze the current package and report errors, but don't build object files");
 }
