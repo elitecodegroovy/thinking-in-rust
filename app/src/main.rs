05c1192d97b1e3b28baa215dbfd43c1f7ab5b124
elitecodegroovy <elite_jigang@163.com>
2023-04-24 23:33:03 +0800

    init app project v5

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,6 +1,41 @@
 use std::io;
 
-fn doPrint() {
+
+#[derive(Debug)]
+struct Person<'a> {
+    name: &'a str,
+    age: u8
+}
+use std::fmt; // Import the `fmt` module.
+
+// Define a structure named `List` containing a `Vec`.
+struct List(Vec<i32>);
+
+impl fmt::Display for List {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Extract the value using tuple indexing,
+        // and create a reference to `vec`.
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // Iterate over `v` in `vec` while enumerating the iteration
+        // count in `count`.
+        for (count, v) in vec.iter().enumerate() {
+            // For every element except the first, add a comma.
+            // Use the ? operator to return on errors.
+            if count != 0 { write!(f, ", ")?; }
+            write!(f, "{}", v)?;
+        }
+
+        // Close the opened bracket and return a fmt::Result value.
+        write!(f, "]")
+    }
+}
+
+
+
+fn do_print() {
     println!("{} days", 31);
 
     // at 0 immediately after the format string.
@@ -52,8 +87,18 @@ fn doPrint() {
     let number: f64 = 1.0;
     let width: usize = 5;
     println!("{number:>width$}");
+
+    let name = "Peter";
+    let age = 27;
+    let peter = Person { name, age };
+
+    // Pretty print
+    println!("{:#?}", peter);
+
+    let v = List(vec![10, 20, 30]);
+    println!("{}", v);
 }
 
 fn main() {
-    doPrint()
+    do_print()
 }
