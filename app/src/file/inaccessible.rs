ac0d599331f64ffa2a105375068a3d66a38281fb
elitecodegroovy <elite_jigang@163.com>
2023-04-30 21:51:43 +0800

    init app project v12

--- app/src/file/inaccessible.rs
+++ app/src/file/inaccessible.rs
@@ -286,7 +286,97 @@ fn do_mutation() {
 
     // `_a` *won't* be `drop`ed again here, because it already has been
     // (manually) `drop`ed
+    let plus_one = make_adder_function(1);
+    assert_eq!(plus_one(2), 3);
+
+    // Instantiate `Unit`
+    let unit = Unit;
+    // Copy `Unit`, there are no resources to move
+    let copied_unit = unit;
+
+    // Both `Unit`s can be used independently
+    println!("original: {:?}", unit);
+    println!("copy: {:?}", copied_unit);
+
+    // Instantiate `Pair`
+    let pair = Pair(Box::new(1), Box::new(2));
+    println!("original: {:?}", pair);
+
+    // Move `pair` into `moved_pair`, moves resources
+    let moved_pair = pair;
+    println!("moved: {:?}", moved_pair);
+
+    // Error! `pair` has lost its resources
+    //println!("original: {:?}", pair);
+    // TODO ^ Try uncommenting this line
+
+    // Clone `moved_pair` into `cloned_pair` (resources are included)
+    let cloned_pair = moved_pair.clone();
+    // Drop the original pair using std::mem::drop
+    drop(moved_pair);
+
+    // Error! `moved_pair` has been dropped
+    //println!("copy: {:?}", moved_pair);
+    // TODO ^ Try uncommenting this line
+
+    // The result from .clone() can still be used!
+    println!("clone: {:?}", cloned_pair);
+    let form = Form {
+        username: "rustacean".to_owned(),
+        age: 28,
+    };
+
+    // If you uncomment this line, you'll get an error saying
+    // "multiple `get` found". Because, after all, there are multiple methods
+    // named `get`.
+    // println!("{}", form.get());
+
+    let username = <Form as UsernameWidget>::get(&form);
+    assert_eq!("rustacean".to_owned(), username);
+    let age = <Form as AgeWidget>::get(&form);
+    assert_eq!(28, age);
+
 }
+trait UsernameWidget {
+    // Get the selected username out of this widget
+    fn get(&self) -> String;
+}
+
+trait AgeWidget {
+    // Get the selected age out of this widget
+    fn get(&self) -> u8;
+}
+
+// A form with both a UsernameWidget and an AgeWidget
+struct Form {
+    username: String,
+    age: u8,
+}
+
+impl UsernameWidget for Form {
+    fn get(&self) -> String {
+        self.username.clone()
+    }
+}
+
+impl AgeWidget for Form {
+    fn get(&self) -> u8 {
+        self.age
+    }
+}
+// A unit struct without resources
+#[derive(Debug, Clone, Copy)]
+struct Unit;
+
+// A tuple struct with resources that implements the `Clone` trait
+#[derive(Clone, Debug)]
+struct Pair(Box<i32>, Box<i32>);
+
+fn make_adder_function(y: i32) -> impl Fn(i32) -> i32 {
+    let closure = move |x: i32| { x + y };
+    closure
+}
+
 struct Droppable {
     name: &'static str,
 }
