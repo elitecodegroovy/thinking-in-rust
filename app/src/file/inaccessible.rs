93495fd2acb475e49c7306be61157272d37e7d7a
elitecodegroovy <elite_jigang@163.com>
2023-04-30 10:08:06 +0800

    init app project v11

--- app/src/file/inaccessible.rs
+++ app/src/file/inaccessible.rs
@@ -162,7 +162,100 @@ pub fn public_function() {
 
     // `boxed_i32` can now give up ownership to `eat_box` and be destroyed
     eat_box_i32(boxed_i32);
+
+    do_mutation();
 }
+
+fn do_mutation() {
+    // Create an immutable Book named `immutabook`
+    let immutabook = Book {
+        // string literals have type `&'static str`
+        author: "Douglas Hofstadter",
+        title: "Gödel, Escher, Bach",
+        year: 1979,
+    };
+    // Create a mutable copy of `immutabook` and call it `mutabook`
+    let mut mutabook = immutabook;
+    // Immutably borrow an immutable object
+    borrow_book(&immutabook);
+    // Immutably borrow a mutable object
+    borrow_book(&mutabook);
+    // Borrow a mutable object as mutable
+    new_edition(&mut mutabook);
+    // Error! Cannot borrow an immutable object as mutable
+    // new_edition(&mut immutabook);
+    // FIXME ^ Comment out this line
+
+    struct Point { x: i32, y: i32, z: i32 }
+
+    let mut point = Point { x: 0, y: 0, z: 0 };
+
+    let borrowed_point = &point;
+    let another_borrow = &point;
+
+    // Data can be accessed via the references and the original owner
+    println!("Point has coordinates: ({}, {}, {})",
+             borrowed_point.x, another_borrow.y, point.z);
+
+    // Error! Can't borrow `point` as mutable because it's currently
+    // borrowed as immutable.
+    // let mutable_borrow = &mut point;
+    // TODO ^ Try uncommenting this line
+
+    // The borrowed values are used again here
+    println!("Point has coordinates: ({}, {}, {})",
+             borrowed_point.x, another_borrow.y, point.z);
+
+    // The immutable references are no longer used for the rest of the code so
+    // it is possible to reborrow with a mutable reference.
+    let mutable_borrow = &mut point;
+
+    // Change data via mutable reference
+    mutable_borrow.x = 5;
+    mutable_borrow.y = 2;
+    mutable_borrow.z = 1;
+
+    // Error! Can't borrow `point` as immutable because it's currently
+    // borrowed as mutable.
+    // let y = &point.y;
+    // TODO ^ Try uncommenting this line
+
+    // Error! Can't print because `println!` takes an immutable reference.
+    // println!("Point Z coordinate is {}", point.z);
+    // TODO ^ Try uncommenting this line
+
+    // Ok! Mutable references can be passed as immutable to `println!`
+    println!("Point has coordinates: ({}, {}, {})",
+             mutable_borrow.x, mutable_borrow.y, mutable_borrow.z);
+
+    // The mutable reference is no longer used for the rest of the code so it
+    // is possible to reborrow
+    let new_borrowed_point = &point;
+    println!("Point now has coordinates: ({}, {}, {})",
+             new_borrowed_point.x, new_borrowed_point.y, new_borrowed_point.z);
+
+
+}
+
+#[derive(Clone, Copy)]
+struct Book {
+    // `&'static str` is a reference to a string allocated in read only memory
+    author: &'static str,
+    title: &'static str,
+    year: u32,
+}
+
+// This function takes a reference to a book
+fn borrow_book(book: &Book) {
+    println!("I immutably borrowed {} - {} edition", book.title, book.year);
+}
+
+// This function takes a reference to a mutable book and changes `year` to 2014
+fn new_edition(book: &mut Book) {
+    book.year = 2014;
+    println!("I mutably borrowed {} - {} edition", book.title, book.year);
+}
+
 
 // This function takes ownership of a box and destroys it
 fn eat_box_i32(boxed_i32: Box<i32>) {
