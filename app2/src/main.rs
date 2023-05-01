ae77965146255dc457e59d00865cca9a51ce62c7
elitecodegroovy <elite_jigang@163.com>
2023-05-01 17:14:01 +0800

    init app project v17

--- app2/src/main.rs
+++ app2/src/main.rs
@@ -239,5 +239,5 @@ async fn main() {
         .with(cors)
         .recover(return_error);
 
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
