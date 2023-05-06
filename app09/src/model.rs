ae4933c462282cfddf7a473bec57a8bb0707f6be
elitecodegroovy <elite_jigang@163.com>
2023-05-06 23:40:54 +0800

    init app project v33

--- app09/src/model.rs
+++ app09/src/model.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
-#[derive(Debug, FromRow, Deserialize, Serialize)]
+#[derive(Debug, FromRow, Serialize)]
 #[allow(non_snake_case)]
 pub struct NoteModel {
     pub id: i32,
@@ -9,8 +9,56 @@ pub struct NoteModel {
     pub content: String,
     pub category: Option<String>,
     pub published: Option<bool>,
-    #[serde(rename = "createdAt")]
+    #[serde(rename = "createdAt", with = "my_date_format")]
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
-    #[serde(rename = "updatedAt")]
+    #[serde(rename = "updatedAt", with = "my_date_format")]
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+mod my_date_format {
+    use chrono::{DateTime, Utc, TimeZone};
+    use serde::{self, Deserialize, Serializer, Deserializer};
+
+    const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S";
+
+    // The signature of a serialize_with function must follow the pattern:
+    //
+    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
+    //    where
+    //        S: Serializer
+    //
+    // although it may also be generic over the input types T.
+    pub fn serialize<S>(
+        date: &Option<DateTime<Utc>>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+    {
+        match date.is_some() {
+            true => {
+                let s = format!("{}", date.as_ref().unwrap().format(FORMAT));
+                serializer.serialize_str(&s)
+            }
+            false => serializer.serialize_none(),
+        }
+
+    }
+
+    // The signature of a deserialize_with function must follow the pattern:
+    //
+    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
+    //    where
+    //        D: Deserializer<'de>
+    //
+    // although it may also be generic over the output types T.
+    pub fn deserialize<'de, D>(
+        deserializer: D,
+    ) -> Result<DateTime<Utc>, D::Error>
+        where
+            D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+    }
 }
