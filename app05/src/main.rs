79e3dfb864a08ae4789d8c46ad4d59a1f9ae89e1
elitecodegroovy <elite_jigang@163.com>
2023-05-17 00:19:03 +0800

    init app project v36

--- app05/src/main.rs
+++ app05/src/main.rs
@@ -8,8 +8,232 @@ mod routes;
 mod store;
 mod types;
 
+use std::mem;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+// A Rectangle can be specified by where its top left and bottom right
+// corners are in space
+#[allow(dead_code)]
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+fn origin() -> Point {
+    Point { x: 0.0, y: 0.0 }
+}
+
+fn boxed_origin() -> Box<Point> {
+    // Allocate this point on the heap, and return a pointer to it
+    Box::new(Point { x: 0.0, y: 0.0 })
+}
+
+fn do_box() {
+    // (all the type annotations are superfluous)
+    // Stack allocated variables
+    let point: Point = origin();
+    let rectangle: Rectangle = Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 }
+    };
+
+    // Heap allocated rectangle
+    let boxed_rectangle: Box<Rectangle> = Box::new(Rectangle {
+        top_left: origin(),
+        bottom_right: Point { x: 3.0, y: -4.0 },
+    });
+
+    // The output of functions can be boxed
+    let boxed_point: Box<Point> = Box::new(origin());
+
+    // Double indirection
+    let box_in_a_box: Box<Box<Point>> = Box::new(boxed_origin());
+
+    println!("Point occupies {} bytes on the stack",
+             mem::size_of_val(&point));
+    println!("Rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&rectangle));
+
+    // box size == pointer size
+    println!("Boxed point occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_point));
+    println!("Boxed rectangle occupies {} bytes on the stack",
+             mem::size_of_val(&boxed_rectangle));
+    println!("Boxed box occupies {} bytes on the stack",
+             mem::size_of_val(&box_in_a_box));
+
+    // Copy the data contained in `boxed_point` into `unboxed_point`
+    let unboxed_point: Point = *boxed_point;
+    println!("Unboxed point occupies {} bytes on the stack",
+             mem::size_of_val(&unboxed_point));
+
+    // Iterators can be collected into vectors
+    let collected_iterator: Vec<i32> = (0..10).collect();
+    println!("Collected (0..10) into: {:?}", collected_iterator);
+
+    // The `vec!` macro can be used to initialize a vector
+    let mut xs = vec![1i32, 2, 3];
+    println!("Initial vector: {:?}", xs);
+
+    // Insert new element at the end of the vector
+    println!("Push 4 into the vector");
+    xs.push(4);
+    println!("Vector: {:?}", xs);
+
+    // Error! Immutable vectors can't grow
+    //collected_iterator.push(0);
+    // FIXME ^ Comment out this line
+
+    // The `len` method yields the number of elements currently stored in a vector
+    println!("Vector length: {}", xs.len());
+
+    // Indexing is done using the square brackets (indexing starts at 0)
+    println!("Second element: {}", xs[1]);
+
+    // `pop` removes the last element from the vector and returns it
+    println!("Pop last element: {:?}", xs.pop());
+
+    // Out of bounds indexing yields a panic
+    //println!("Fourth element: {}", xs[3]);
+    // FIXME ^ Comment out this line
+
+    // `Vector`s can be easily iterated over
+    println!("Contents of xs:");
+    for x in xs.iter() {
+        println!("> {}", x);
+    }
+
+    // A `Vector` can also be iterated over while the iteration
+    // count is enumerated in a separate variable (`i`)
+    for (i, x) in xs.iter().enumerate() {
+        println!("In position {} we have value {}", i, x);
+    }
+
+    // Thanks to `iter_mut`, mutable `Vector`s can also be iterated
+    // over in a way that allows modifying each value
+    for x in xs.iter_mut() {
+        *x *= 3;
+    }
+    println!("Updated vector: {:?}", xs);
+
+    do_str();
+}
+
+fn do_str() {
+    // (all the type annotations are superfluous)
+    // A reference to a string allocated in read only memory
+    let pangram: &'static str = "the quick brown fox jumps over the lazy dog";
+    println!("Pangram: {}", pangram);
+
+    // Iterate over words in reverse, no new string is allocated
+    println!("Words in reverse");
+    for word in pangram.split_whitespace().rev() {
+        println!("> {}", word);
+    }
+
+    // Copy chars into a vector, sort and remove duplicates
+    let mut chars: Vec<char> = pangram.chars().collect();
+    chars.sort();
+    chars.dedup();
+
+    // Create an empty and growable `String`
+    let mut string = String::new();
+    for c in chars {
+        // Insert a char at the end of string
+        string.push(c);
+        // Insert a string at the end of string
+        string.push_str(", ");
+    }
+
+    // The trimmed string is a slice to the original string, hence no new
+    // allocation is performed
+    let chars_to_trim: &[char] = &[' ', ','];
+    let trimmed_str: &str = string.trim_matches(chars_to_trim);
+    println!("Used characters: {}", trimmed_str);
+
+    // Heap allocate a string
+    let alice = String::from("I like dogs");
+    // Allocate new memory and store the modified string there
+    let bob: String = alice.replace("dog", "cat");
+
+    println!("Alice says: {}", alice);
+    println!("Bob says: {}", bob);
+
+    // You can use escapes to write bytes by their hexadecimal values...
+    let byte_escape = "I'm writing \x52\x75\x73\x74!";
+    println!("What are you doing\x3F (\\x3F means ?) {}", byte_escape);
+
+    // ...or Unicode code points.
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!("Unicode character {} (U+211D) is called {}",
+             unicode_codepoint, character_name );
+
+
+    let long_string = "String literals
+                        can span multiple lines.
+                        The linebreak and indentation here ->\
+                        <- can be escaped too!";
+    println!("{}", long_string);
+
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{}", raw_str);
+
+    // If you need quotes in a raw string, add a pair of #s
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // If you need "# in your string, just use more #s in the delimiter.
+    // You can use up to 65535 #s.
+    let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", longer_delimiter);
+
+    // Note that this is not actually a `&str`
+    let bytestring: &[u8; 21] = b"this is a byte string";
+
+    // Byte arrays don't have the `Display` trait, so printing them is a bit limited
+    println!("A byte string: {:?}", bytestring);
+
+    // Byte strings can have byte escapes...
+    let escaped = b"\x52\x75\x73\x74 as bytes";
+    // ...but no unicode escapes
+    // let escaped = b"\u{211D} is not allowed";
+    println!("Some escaped bytes: {:?}", escaped);
+
+
+    // Raw byte strings work just like raw strings
+    let raw_bytestring = br"\u{211D} is not escaped here";
+    println!("{:?}", raw_bytestring);
+
+    use std::str;
+    // Converting a byte array to `str` can fail
+    if let Ok(my_str) = str::from_utf8(raw_bytestring) {
+        println!("And the same as text: '{}'", my_str);
+    }
+
+    let _quotes = br#"You can also use "fancier" formatting, \
+                    like with normal raw strings"#;
+
+    // Byte strings don't have to be UTF-8
+    let shift_jis = b"\x82\xe6\x82\xa8\x82\xb1\x82\xbb"; // "ようこそ" in SHIFT-JIS
+
+    // But then they can't always be converted to `str`
+    match str::from_utf8(shift_jis) {
+        Ok(my_str) => println!("Conversion successful: '{}'", my_str),
+        Err(e) => println!("Conversion failed: {:?}", e),
+    };
+}
+
 #[tokio::main]
 async fn main() {
+    do_box();
     // let log_filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "practical_rust_book=info,warp=error".to_owned());
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
     log::info!("app booting up ");
