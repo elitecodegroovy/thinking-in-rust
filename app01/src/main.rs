b4e7f3ea7f809e2b08fde54e2dfa52732b53e5c2
elitecodegroovy <elite_jigang@163.com>
2023-05-16 23:59:55 +0800

    init app project v35

--- app01/src/main.rs
+++ app01/src/main.rs
@@ -852,7 +852,23 @@ fn drink(beverage: &str){
     if beverage == "lemonade"{ ah();}
     else{println!("Some refreshing {} is all I need.", beverage);}
 }
+use std::num::ParseIntError;
 
+// As with `Option`, we can use combinators such as `map()`.
+// This function is otherwise identical to the one above and reads:
+// Multiply if both values can be parsed from str, otherwise pass on the error.
+fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    first_number_str.parse::<i32>().and_then(|first_number| {
+        second_number_str.parse::<i32>().map(|second_number| first_number * second_number)
+    })
+}
+
+fn print(result: Result<i32, ParseIntError>) {
+    match result {
+        Ok(n)  => println!("n is {}", n),
+        Err(e) => println!("Error: {}", e),
+    }
+}
 fn define_macro() {
     say_hello!();
     foo();
@@ -873,6 +889,26 @@ fn define_macro() {
     drink("water");
     drink("lemonade");
     // rustc  lemonade.rs -C panic=abort
+
+
+
+    // This still presents a reasonable answer.
+    let twenty = multiply("10", "2");
+    print(twenty);
+
+    // The following now provides a much more helpful error message.
+    let tt = multiply("t", "2");
+    print(tt);
+
+    // ？ introduce
+    // try!()  macro -> catch Result<T, parseIntError>
+
+    let strings = vec!["tofu", "93", "18"];
+    let numbers: Vec<_> = strings
+        .into_iter()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .collect();
+    println!("Results: {:?}", numbers);
 }
 
 
