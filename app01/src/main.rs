616b2668397fb78c7d9a5f7459dc0f580196352c
elitegroovycode <elite_jigang@163.com>
2024-05-02 20:45:47 +0800

    feat: variable definition

--- app01/src/main.rs
+++ app01/src/main.rs
@@ -786,12 +786,14 @@ fn do_print() {
     do_closure();
 }
 
-fn entry_point() {
+fn exec_variable() {
     let mut ticket = 0;   // 初始化定义
     println!("init ticket count: {}", ticket); // 打印操作
     ticket += 1;  // 逻辑增加1
     println!("update ticket count: {}", ticket); // 打印操作
-
+}
+fn entry_point() {
+    exec_variable();
     do_print();
     for _ in 0..10 {
         println!("uuid: {}", Uuid::new_v4().hyphenated().to_string().replace("-", ""));
@@ -927,5 +929,6 @@ async fn main() {
     entry_point();
 
     let hello = warp::get().map(|| format!("Hello, World!"));
-    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(hello).run(([0, 0, 0, 0], 3030)).await;
 }
+
